2333b4e1ecdf7a956919e7f1eb5436f6a537cac1
jvos-cs <joaovitordeos@gmail.com>
2023-04-05 18:48:43 -0300

    feat

--- ex_files.c
+++ ex_files.c
@@ -22,46 +22,64 @@ int num_caracteres(FILE *a){
     return numChar;
 }
 
-/* Ex.2 Converte uma string em float. */
-float converte_str_flt(char *s){
-    float num;
-    int i;
+///* Ex.2 Converte uma string em float. */
+//float converte_str_flt(char *s){
+//    float num;
+//    int i;
+//
+//    num = 0;
+//    for (i=0; s[i] != NULL_CHAR ; i++)
+//        num = num*10 + (s[i] - 48);
+//
+//    return num;
+//}
+//
+//
+///* Ex.2 Retorna a media do valor dos inteiros de cada linha do arquivo. */
+//float media_arqv(FILE *a){
+//    float media, acc; 
+//    char line[MAX_LINE+1];
+//    int i; 
+//
+//
+//    i = 1;
+//    acc = 0;
+//
+//    fgets(line, MAX_LINE, a);
+//    line[strcspn(line, "\n")] = 0;
+//
+//    acc += converte_str_flt(line);
+//    while ( !feof(a) ){
+//        printf("%s", line);
+//        fgets(line, MAX_LINE, a);
+//        line[strcspn(line, "\n")] = 0;
+//        acc += converte_str_flt(line);
+//        i++;
+//    }
+//    
+//    media = acc / i;
+//
+//    return media;
+//}
 
-    num = 0;
-    for (i=0; s[i] != NULL_CHAR ; i++)
-        num = num*10 + (s[i] - 48);
-
-    return num;
-}
-
-
-/* Ex.2 Retorna a media do valor dos inteiros de cada linha do arquivo. */
+/* Ex.2(Corrigido) */
 float media_arqv(FILE *a){
-    float media, acc; 
-    char line[MAX_LINE+1];
-    int i; 
+    float media, numLinha, acc, valAtual;
 
-
-    i = 1;
+    numLinha = 0;
     acc = 0;
 
-    fgets(line, MAX_LINE, a);
-    line[strcspn(line, "\n")] = 0;
-
-    acc += converte_str_flt(line);
+    numLinha +=  fscanf(a, "%f", &valAtual);
+    acc += valAtual;
     while ( !feof(a) ){
-        printf("%s", line);
-        fgets(line, MAX_LINE, a);
-        line[strcspn(line, "\n")] = 0;
-        acc += converte_str_flt(line);
-        i++;
+        numLinha +=  fscanf(a, "%f", &valAtual);
+        acc += valAtual;
     }
-    
-    media = acc / i;
+
+    media = acc/numLinha; 
 
     return media;
 }
-
 
 /* Verifica se o caractere eh minusculo. */
 int verifica_char(char c){
@@ -91,17 +109,18 @@ int converte_txt_maiusc(FILE *a1, FILE *a2){
 int main (){
     float media;
     int numChar;
-    FILE *arqIn, *arqOut;
+    FILE *arqIn;// *arqOut;
 
-    if ( !(arqIn = fopen("poema", "r")) )
+    if ( !(arqIn = fopen("numeros", "r")) )
         return 1;
     if ( !(arqOut = fopen("maiusc", "w")) )
         return 1;
 
     
 
-    //numChar = num_caracteres(a);
-    //media = media_arqv(a);
+    //numChar = num_caracteres(arqIn);
+    //rewind(arqIn); // retorna o ponteiro da stream para o local inicial
+    //media = media_arqv(arqIn);
     
     //printf("Num de caracteres: %d\n", numChar);
     //printf("media do val do arqv: %f\n", media);
