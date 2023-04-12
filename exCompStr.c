57363f645ca01efa5d07f6ef522ce7d2d7a28954
João Vitor de Oliveira <83550583+jvos-cs@users.noreply.github.com>
2023-04-12 12:57:02 -0300

    feat

--- exCompStr.c
+++ exCompStr.c
@@ -1,36 +1,42 @@
 /* 
- * Exercicios da disciplina de Prog2 - Manipulacao de Strings
+ * Exercicios Complementares da disciplina de Prog2 - Manipulacao de Strings
  * Autor: Joao Vitor de Oliveira Souza
  * 27/03/23
- * 1,2,7,9,10
- *
  * 
  * 1. (posPal) Faça um programa que leia pelo teclado um texto com pelo menos 20  palavras
  * (cada palavra com pelo menos 5 caracteres) separadas por ESPAÇO. Em seguida o programa
  * deve ler do usuário uma letra ou dígito e então mostrar a posição de todas as palavras
  * no texto que começam com a letra ou dígito informados. Numere a posição das palavras a partir
  * de 0 (zero). 
- *   
+ *
  * 2. (primPal) Usando o resultado do exercício anterior, faça um programa que leia pelo teclado
  * um texto com pelo menos 20 palavras (cada palavra com pelo menos 5 caracteres) separadas por
  * ESPAÇO. Em seguida o programa deve mostrar uma relação contendo em cada linha a primeira letra
  * de uma palavra seguida da posição de todas as palavras que começam com aquela letra.  
  */
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define STR_MAX 25
-#define TXT_MAX 500
-
-#define NEW_LINE 10
-#define NULL_CHR 0
+#define TAMSTR 25
+#define TAMTXT 1024
+#define NULL_CHAR 0
+#define a_CHAR 97
+#define z_CHAR 122
+#define A_CHAR 65
+#define Z_CHAR 90
+#define ZERO_CHAR 48
+#define NINE_CHAR 57
 #define SPACE 32
+#define OP_BRK 91
+#define CL_BRK 93
 
-typedef struct lista {
+struct lista {
     struct lista *prox;
     char *str;
-} lista_t;
+};
+
+typedef struct lista lista_t;
 
 /* Inicia a lista de strings, retorna um ponteiro para o primeiro elemento da lista,
    Se falhar, retorna NULL. */
@@ -67,7 +73,7 @@ lista_t *destroi_lista (lista_t *s){
 }
 
 /* Adiciona uma string a lista. */
-int adiciona_str(lista_t *s, char *str){
+int strToLista(lista_t *s, char *str){
     lista_t *prox, *ante, *l;
 
     // Se o ponteiro da str do primeiro no da lista for NULL, atribui a string a ele
@@ -76,6 +82,7 @@ int adiciona_str(lista_t *s, char *str){
         return 1;
     }
 
+    // Adiciona novo no a lista
     if ( !(l = cria_no_lista()) )
         return 0;
 
@@ -93,39 +100,55 @@ int adiciona_str(lista_t *s, char *str){
     return 1;
 }
 
-/* Le strings e as insere numa lista de strings. Cada string eh separada por espacos em branco. */
-int le_strings(lista_t *s){
-    char *txt, *str, c;
+/* Aloca uma string e retorna um ponteiro para a mesma. Se falhar retorna NULL */
+char *alocaStr(){
+    char *str;
+    if ( !(str = malloc(sizeof(char) * TAMSTR)) )
+        return NULL;
+    return str;
+}
+
+/* Extrai uma parte do texto iniciada no indice 'idc' como uma string (com o fim dessa string sendo
+   considerado como espaco). Retorna o indice onde a string acaba no txt (onde foi encontrado o espaco). */
+int extractStr(char *txt, char *str, int idc){
+    int j;
+
+    j = 0;
+    while ( (txt[idc] != SPACE) && (txt[idc] != NULL_CHAR) ){   // Enquanto nao encontrar um espaco ou char nulo 
+        str[j] = txt[idc];                                      // Continua inserindo os caracteres em str formando
+        idc++;                                                  // uma nova string.
+        j++;
+    }
+    str[j++] = NULL_CHAR;   // No final da string eh inserido um char nulo.
+    
+    return idc;
+}
+
+/* Adiciona cada string do texto em uma lista (considerando que cada str esta separada por espaco). */
+int txtToList(lista_t *s, char *txt){
+    char *str, c;
     int i, j;
-
-    if ( !(txt = malloc(sizeof(char)*TXT_MAX)) )
-        return 0;
-
-    fgets(txt, TXT_MAX, stdin);
-    txt[strcspn(txt, "\n")] = '\0';
 
     i = 0;
     c = txt[i];
 
-    if ( !(str = malloc(sizeof(char)*STR_MAX)) )
+    if ( !(str = alocaStr()) )
         return 0;
 
-    while (c != NULL_CHR){
-        for (j=0;txt[i]!=SPACE && txt[i]!=NULL_CHR; i++, j++)
-            str[j] = txt[i];
-    
-        str[j++] = NULL_CHR; 
-        
-        adiciona_str(s, str);
+    while (c != NULL_CHAR){
+        j = 0;
 
-        if ( txt[i]!= NULL_CHR )
-            if ( !(str = malloc(sizeof(char)*STR_MAX)) )
+        i = extractStr(txt, str, i);          // Extrai a proxima string do texto
+    
+        strToLista(s, str);                   // Adiciona a string a lista
+
+        if ( txt[i] != NULL_CHAR )            // Aloca uma nova string se nao for o fim do texto
+            if ( !(str = alocaStr()) ) 
                 return 0;
     
         c = txt[i];
         i++;
     }
-    free(txt);
     return 1;
 }
 
@@ -135,40 +158,61 @@ int busca_strings(lista_t *s, char c){
     int i;
     
     i = 0;
-
     prox = s->prox;
+    
     if (s->str[0] == c)
         printf("%d ", i);
+    
     i++;
-    while (prox!=NULL_CHR){
+    while (prox != NULL){
         if (prox->str[0] == c)
             printf("%d ", i);
         prox = prox->prox;
         i++;
     }
+    printf("\n");
 
     return 1;
 }
 
-int main (){
-    lista_t *s, *prox; 
+/* Para cada string de uma lista sera exibido a posicao de cada ocorrencia de outras strings com o
+   mesmo caractere inicial. */
+void imprimeRelacao(lista_t *l){
+    lista_t *prox;
     char c;
 
-    s = cria_no_lista();
+    prox = l->prox;
+    c = l->str[0];
 
-    le_strings(s);
-
-    //scanf("%c", &c);
-    //busca_strings(s, c);
-
-
-    while (prox != NULL)
-    {
-        /* code */
-    }
+    printf("%c: ", c);
+    busca_strings(l, c);
     
+    while (prox != NULL){
+        c = prox->str[0];
+        printf("%c: ", c);
+        busca_strings(l, c);
 
-    s = destroi_lista(s);
+        prox = prox->prox;
+    }
+}
 
+
+int main(int argc, char *argv[]){
+    lista_t *l;
+    char *txt;
+
+    if ( !(txt = malloc(sizeof(char) * TAMTXT)) )
+        return 1;
+
+    fgets(txt, TAMTXT, stdin);
+    txt[strcspn(txt, "\n")] = '\0'; // Insere um caractere nulo no lugar da quebra de linha no final do texto
+
+    l = cria_no_lista();
+
+    txtToList(l, txt);
+    imprimeRelacao(l);
+    
+    l = destroi_lista(l);
+    free(txt);
     return 0;
 }
